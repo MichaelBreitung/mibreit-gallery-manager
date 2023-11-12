8588db42e695150e07a02b218561101ea0329c38
MichaelBreitung <mibreit@gmx.de>
2023-11-12 13:09:52 +0100

    minor: added template files

--- modules/gallery_create.py
+++ modules/gallery_create.py
@@ -1,6 +1,6 @@
 import re
 from shutil import copy
-from os import mkdir, path, getcwd
+from os import mkdir, path
 
 allowedCharacters = re.compile(r'[a-zA-Z-]*$')
 
@@ -14,7 +14,7 @@ class GalleryCreator:
         if allowedCharacters.match(gallery_name):
             if not path.isdir(gallery_name):
                 full_gallery_path = path.join(self.__galleries_path, gallery_name)
-                script_path = getcwd()
+                script_path = path.join(path.dirname(path.abspath(__file__)), "..")
                 mkdir(full_gallery_path)
                 mkdir(f"{full_gallery_path}/images")
                 mkdir(f"{full_gallery_path}/small")
